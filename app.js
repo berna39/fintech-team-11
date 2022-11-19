a335110fd1b1caacddd3ca960ce8a62f42ec4c8d
berna39 <kalemajoseph39@gmail.com>
2022-11-19 20:44:20 +0200

    Initital commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotnenv = require('dotenv');
+const app = express();
+
+dotnenv.config();
+
+const PORT = process.env.PORT || 2500;
+
+app.use(express.json());
+
+app.get('/', async(req, res) => {
+    res.send({message: 'This is the fintech team 11'});
+});
+
+app.listen(PORT, (err) => {
+    console.log(`App up and running on ${PORT}`);
+});
