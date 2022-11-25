fb50c13bcb90d2ea2b3e54a3993ffcf2204190a1
berna39 <kalemajoseph39@gmail.com>
2022-11-25 10:24:08 +0200

    app initialization

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const dotnenv = require('dotenv');
+const session = require('express-session');
 const app = express();
-const db = require('./db/db');
 
 dotnenv.config();
+
+// middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(session({
+    secret: 'my secret',
+    saveUninitialized: true,
+    resave: false
+}));
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message
+    next();
+});
+
+app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 2500;
 
